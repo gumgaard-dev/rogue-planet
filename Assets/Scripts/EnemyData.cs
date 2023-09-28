dec5c6a9eb401642fea994b45619209ffc5a1fd8
Kaitlyn <97715354+archambaultkm@users.noreply.github.com>
2023-09-28 16:45:43 -0300

    started on enemy

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemy", order = 1)]
+public class EnemyData : ScriptableObject
+{
+    public int health;
+    public int damage;
+    public float speed;
+
+}
