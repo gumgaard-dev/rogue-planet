62d301ab9c39b277234d6450bf501cde441449da
Ewan Baxter <ewan@toughbaxter.com>
2023-10-03 21:52:12 -0300

    comment outlining transition to idle state

--- Assets/Scripts/Player States/PlayerMoveState.cs
+++ Assets/Scripts/Player States/PlayerMoveState.cs
@@ -25,6 +25,7 @@ namespace Capstone
             Vector2 newVelocity = Player.Velocity;
 
             // Smoothly changes the player's velocity
+            // target velocity is input.x * run speed, becuase info.x is either -1, 0, or 1 based on input
             newVelocity.x = Mathf.SmoothDamp(
                 Player.Velocity.x,
                 InputInfo.Move.x * Settings.RunSpeed,
@@ -33,6 +34,12 @@ namespace Capstone
             );
 
             Player.SetVelocity( newVelocity );
+
+            // This should set the state to idle only when the player is still.
+            //if (newVelocity.x == 0)
+            //{
+            //    Player.SetState(PlayerStateType.Idle);
+            //}
         }
     }
 }
