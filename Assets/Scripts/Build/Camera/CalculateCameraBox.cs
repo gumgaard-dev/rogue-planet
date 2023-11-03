1c146bbd3cad8e3fc7f6f2991d5a31346b23fee0
Riley HP <rhend@proton.me>
2023-11-03 16:32:49 -0300

    added parallax

--- Assets/Scripts/Build/Camera/CalculateCameraBox.cs
+++ Assets/Scripts/Build/Camera/CalculateCameraBox.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class CalculateCameraBox : MonoBehaviour
 {
     private Camera _camera;
-    void Start()
+    private BoxCollider2D _boxCollider;
+    private float _sizeX, _sizeY, _xyRatio;
+    
+    void Awake()
     {
         if (!TryGetComponent(out _camera))
         {
             Debug.Log("CameraCalculateBox: no camera detected, please attach this script to a camera object");
         }
-    }
 
-    // Update is called once per frame
-    void Update()
+        if(!TryGetComponent(out _boxCollider))
+        {
+            Debug.Log("CameraCalculateBox: no box collider detected, please attach one in the inspector");
+
+        }
+    }
+    
+    void Start()
     {
-        
+        _xyRatio = (float)Screen.width / Screen.height;
+        _sizeY = _camera.orthographicSize * 2;
+        _sizeX = _sizeY * _xyRatio;
+        _boxCollider.size = new Vector2(_sizeX, _sizeY);
     }
 }
