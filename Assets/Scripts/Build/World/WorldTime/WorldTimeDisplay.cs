0f39ddf42a8194eb02088d4380f5916b07eb7bf3
sarah <sarah.annealice@gmail.com>
2023-11-07 13:44:19 -0400

    ui complete

--- Assets/Scripts/Build/World/WorldTime/WorldTimeDisplay.cs
+++ Assets/Scripts/Build/World/WorldTime/WorldTimeDisplay.cs
@@ -11,32 +11,35 @@ namespace Build.World.WorldTime
         public Image imgSun;
         public Image imgMoon;
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-            // check that WorldTime script is assigned
-            if (worldTime != null)
+            imgSun.gameObject.SetActive(true);
+            imgMoon.gameObject.SetActive(false);
+            timeOfDaySlider.value = 0.5f; 
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            // get the timeOfDay value from WorldTime script
+            var timeOfDay = worldTime.timeOfDay;
+            
+            // calculate slider value based on time of day (decreasing as night approaches)
+            var sliderValue = 1f - timeOfDay;
+            
+            // updates slider value
+            timeOfDaySlider.value = sliderValue;
+            
+            // update image
+            if (worldTime.isDay)
             {
-                // get the timeOfDay value from WorldTime script
-                float timeOfDay = worldTime.timeOfDay;
-                
-                // calculate slider value based on time of day (decreasing as night approaches)
-                float sliderValue = 1f - timeOfDay;
-                
-                // updates slider value
-                timeOfDaySlider.value = sliderValue;
-                
-                // update image
-                if (sliderValue <= 0.5f)
-                {
-                    imgSun.gameObject.SetActive(false);
-                    imgMoon.gameObject.SetActive(true);
-                }
-                else
-                {
-                    imgSun.gameObject.SetActive(true);
-                    imgMoon.gameObject.SetActive(false);
-                }
+                imgSun.gameObject.SetActive(true);
+                imgMoon.gameObject.SetActive(false);
+            }
+            else
+            {
+                imgSun.gameObject.SetActive(false);
+                imgMoon.gameObject.SetActive(true);
             }
         }
     }
